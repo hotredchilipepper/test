37ce3a0cd481d503bd83f5f7e8e9e5f0532053c3
Юрий <68888570+hotredchilipepper@users.noreply.github.com>
2020-12-15 02:17:54 +0700

    Update translit.py

--- translit.py
+++ translit.py
@@ -1,14 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/a', methods=['POST','GET'])
+def create_text():
+    if request.method == "POST":
+        text = request.form['name']
+    else:
+        return render_template("index.html")
+
 def translit(text):
     cyrillic = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
     latin = 'a|b|v|g|d|e|e|zh|z|i|i|k|l|m|n|o|p|r|s|t|u|f|h|tc|ch|sh|shch||y||e|iu|ia'.split(
         '|')
     tab = {k: v for k, v in zip(cyrillic, latin)}
-    newtext = ''
+    newtext: str = ''
     for ch in text:
         func = str.capitalize if ch.isupper() else str.lower
         newtext += func(tab.get(ch.lower(), ch))
     return newtext
 
-text = 'привет мир'
-print(translit(text))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
